be961e5b05cdd2dc0d3a790ccfa050c1df698e57
aaruhisharma <aarohisharma1144@gmail.com>
2023-07-08 11:35:28 +0530

    about variable

---
+++ example9.js
@@ -0,0 +1,4 @@
+let firstName = prompt("What is your first name?");
+console.log("Hi,"+firstName+"!");
+console.log("How are you doing, "+firstName+"?");
+console.log("See you later,"+firstName+"!");
