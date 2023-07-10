79167991fc2b3d53195cb2c910fad18c92548ff1
aaruhisharma <aarohisharma1144@gmail.com>
2023-07-10 09:52:01 +0530

    about variable

---
+++ example16.js
@@ -0,0 +1,5 @@
+let firstName = "'Aaruhi Sharma'";
+console.log(firstName);
+// let first_name = "${'Aaruhi Sharma'}";
+let first_name = `${'Aaruhi Sharma'}`;
+console.log(first_name);
---
+++ example18.js
@@ -0,0 +1,2 @@
+let thing
+coonsol.log(thing);
