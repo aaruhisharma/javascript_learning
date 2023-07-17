015b0073d87f1fe685590c98860c2bc7e574150a
aaruhisharma <aarohisharma1144@gmail.com>
2023-07-17 12:27:46 +0530

    about nested object

---
+++ example68.js
@@ -0,0 +1,12 @@
+let school = {
+    georgiaInstitueOfTechnology :{
+        address : "North Ave NW , Atlanta , GA 30332",
+        PhoneNumber : "(404) 894-2000",
+        dataEstablished  : "October13,1885"
+
+        
+    }
+
+};
+
+console.log(school);
