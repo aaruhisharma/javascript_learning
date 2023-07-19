27b2d04c275430991855016fcc3970e2f3ffb8cf
aaruhisharma <aarohisharma1144@gmail.com>
2023-07-19 11:50:04 +0530

    about function

--- example73.js
+++ example73.js
@@ -1,4 +1,4 @@
 function displayCities(){
-   let citiesLivedIn : "Seattle","Providence","New york";
+   letcitiesLivedIn : ["Seattle","Providence","New york"];
 }
 console.log(displayCities.citiesLivedIn[1]);
